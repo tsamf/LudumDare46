c12ffb363216757f9165bcda29b4fdcc4d1917f8
samfordt <samfordt@gmail.com>
2020-04-18 12:37:44 -0500

    Created building prefabs and working on generating a level.

---
+++ LudumDare46/Assets/Scripts/GenerateLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateLevel : MonoBehaviour
+{
+    public List<GameObject> buildings = new List<GameObject>();
+    public float Xoffset = 0.0f;
+    public float yOffset = 2.5f;
+    public float zOffset = -3.0f;
+    void Start()
+    {
+        Random.InitState(42);
+
+        //Generate Buildings
+        for(int i = 0; i <1000; i++)
+        {
+            int index = Random.Range(0, 3);
+
+            GameObject building = Instantiate(buildings[index],new Vector3(Xoffset,yOffset,zOffset),Quaternion.identity);
+
+            float size = building.GetComponent<Building>().size;
+        }
+    }
+}
---
+++ LudumDare46/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public float movementSpeed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 CurrentPosition = transform.position;
+        CurrentPosition.x = CurrentPosition.x + movementSpeed * Time.deltaTime;
+        transform.position = CurrentPosition;
+    }
+}
