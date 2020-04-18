5de362ffc64276521495fbf6737d9a9bc40a5e93
samfordt <samfordt@gmail.com>
2020-04-18 13:17:28 -0500

    Got buildings to spawn in level.

--- LudumDare46/Assets/Scripts/GenerateLevel.cs
+++ LudumDare46/Assets/Scripts/GenerateLevel.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 public class GenerateLevel : MonoBehaviour
 {
     public List<GameObject> buildings = new List<GameObject>();
-    public float Xoffset = 0.0f;
+    public float Xoffset = -3.0f;
     public float yOffset = 2.5f;
-    public float zOffset = -3.0f;
+    public float zOffset = 1.0f;
+    public float spaceBetweenBuildings = 3.0f;
+
     void Start()
     {
-        Random.InitState(42);
+        Random.InitState(45);
 
         //Generate Buildings
         for(int i = 0; i <1000; i++)
         {
+
             int index = Random.Range(0, 3);
 
+            float size = buildings[index].GetComponent<Building>().size;
+
+            Xoffset += size / 2.0f;
+           
             GameObject building = Instantiate(buildings[index],new Vector3(Xoffset,yOffset,zOffset),Quaternion.identity);
 
-            float size = building.GetComponent<Building>().size;
+            Xoffset += size / 2.0f + 3.0f;
         }
     }
 }
