ba0fee8da5554421cee6935c1ecadcf37fdf237b
samfordt <samfordt@gmail.com>
2020-04-19 09:49:16 -0500

    Cleaned up level code.

--- LudumDare46/Assets/Scripts/GenerateLevel.cs
+++ LudumDare46/Assets/Scripts/GenerateLevel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GenerateLevel : MonoBehaviour
 {
     public List<GameObject> buildings = new List<GameObject>();
+    public List<GameObject> enemies = new List<GameObject>();
     public float Xoffset = -3.0f;
     public float yOffset = 2.5f;
     public float zOffset = 1.0f;
@@ -17,16 +18,14 @@ public class GenerateLevel : MonoBehaviour
         //Generate Buildings
         for(int i = 0; i <1000; i++)
         {
-
             int index = Random.Range(0, 3);
-
             float size = buildings[index].GetComponent<Building>().size;
-
             Xoffset += size / 2.0f;
-           
             GameObject building = Instantiate(buildings[index],new Vector3(Xoffset,yOffset,zOffset),Quaternion.identity);
-
             Xoffset += size / 2.0f + 3.0f;
+
+
+
         }
     }
 }
