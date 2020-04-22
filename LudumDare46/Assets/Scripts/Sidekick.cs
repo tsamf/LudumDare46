40f0698d046d11db8473a1c17e9fbbccb80df6d6
bklolo <brandonlovatt@gmail.com>
2020-04-22 18:30:10 -0500

    Revamped Sidekick (needs shooting capabilities) -Added new script to prefab -Added Animations: Idle, Sprint, Leap, Fall (animations need tweaking)

---
+++ LudumDare46/Assets/Scripts/Sidekick.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Sidekick : MonoBehaviour
+{
+    // Animator vars
+    private Animator animator;
+    public Rigidbody rb;
+    public float movespeed = 2f;
+
+    // jump vars
+    bool grounded = false;
+    Collider[] groundCollisions;
+    float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float jumpHeight = 35.0f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (grounded && Input.GetAxis("Jump") > 0)
+        {
+            grounded = false;
+            animator.SetBool("grounded", grounded);
+            rb.AddForce(new Vector3(0, jumpHeight, 0));
+            Debug.Log(jumpHeight.ToString());
+        }
+
+        groundCollisions = Physics.OverlapSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (groundCollisions.Length > 0)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        animator.SetBool("grounded", grounded);
+
+        float move = Input.GetAxis("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(move));
+
+        rb.velocity = new Vector3(move * movespeed, rb.velocity.y, 0);
+
+    }
+
+}
